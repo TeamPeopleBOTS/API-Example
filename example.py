66771b6f0812e130fed93fe1496ff6c956b9c473
eaterwapi <61935940+eaterwapi@users.noreply.github.com>
2020-03-19 11:40:26 +0800

    Update example.py

--- example.py
+++ example.py
@@ -32,79 +32,79 @@ def sendMessage(to, text):
 	return req.json()
 
 def sendMention(to, text, userids=[]):
-	url = host + '/sendMention'
-	data = {
-		'chat_id': to,
-		'message': text,
-		'user_ids': userids
-	}
-	req = requests.post(url, data=data, headers=headers)
-	return req.json()
+    url = host + '/sendMention'
+    data = {
+        'chat_id': to,
+        'message': text,
+        'user_ids': userids
+    }
+    req = requests.post(url, data=data, headers=headers)
+    return req.json()
 
 def sendMedia(to, path, caption=''):
-	url = host + '/sendMedia'
-	data = {
-		'chat_id': to,
-		'caption': caption
-	}
-	files ={'files': open(path,'rb')}
-	req = requests.post(url, data=data, files=files, headers=headers)
-	return req.json()
+    url = host + '/sendMedia'
+    data = {
+        'chat_id': to,
+        'caption': caption
+    }
+    files ={'files': open(path,'rb')}
+    req = requests.post(url, data=data, files=files, headers=headers)
+    return req.json()
 
 def setName(name):
-	url = host + '/setMyName'
-	data = {
-		'name': name
-	}
-	req = requests.post(url, data=data, headers=headers)
-	return req.json()
+    url = host + '/setMyName'
+    data = {
+        'name': name
+    }
+    req = requests.post(url, data=data, headers=headers)
+    return req.json()
 
 def setBio(bio):
-	url = host + '/setMyStatus'
-	data = {
-		'status': bio
-	}
-	req = requests.post(url, data=data, headers=headers)
-	return req.json()
+    url = host + '/setMyStatus'
+    data = {
+        'status': bio
+    }
+    req = requests.post(url, data=data, headers=headers)
+    return req.json()
 
-def getBio(chat_id):
-	url = host + '/getBio'
-	data = {
-		'chat_id': chat_id
-	}
-	req = requests.get(url, data=data, headers=headers)
-	return req.json()
+def getBio(user_id):
+    url = host + '/getBio'
+    params = {
+        'chat_id': user_id
+    }
+    req = requests.get(url, params=params, headers=headers)
+    return req.json()
 
 def getContacts():
-	url = host + '/getContacts'
-	req = requests.get(url, headers=headers)
-	return req.json()
+    url = host + '/getContacts'
+    req = requests.get(url, headers=headers)
+    return req.json()
 
 def getMyContacts():
-	url = host + '/getMyContacts'
-	req = requests.get(url, headers=headers)
-	return req.json()
+    url = host + '/getMyContacts'
+    req = requests.get(url, headers=headers)
+    return req.json()
 
 def blockContact(user_id):
-	url = host + '/blockContact'
-	data = {
-		'chat_id': user_id
-	}
-	req = requests.post(url, data=data, headers=headers)
-	return req.json()
+    url = host + '/blockContact'
+    data = {
+        'chat_id': user_id
+    }
+    req = requests.post(url, data=data, headers=headers)
+    return req.json()
 
 def unblockContact(user_id):
-	url = host + '/unblockContact'
-	data = {
-		'chat_id': user_id
-	}
-	req = requests.post(url, data=data, headers=headers)
-	return req.json()
+    url = host + '/unblockContact'
+    data = {
+        'chat_id': user_id
+    }
+    req = requests.post(url, data=data, headers=headers)
+    return req.json()
 
 def getChats():
-	url = host + '/getChats'
-	req = requests.get(url, headers=headers)
-	return req.json()
+    url = host + '/getChats'
+    req = requests.get(url, headers=headers)
+    return req.json()
 
 def getChat(to):
     url = host + '/getChatsinchat'
@@ -115,63 +115,63 @@ def getChat(to):
     return req.json()
 
 def sendSeen(to):
-	url = host + '/sendSeen'
-	data = {
-		'chat_id': to
-	}
-	req = requests.post(url, data=data, headers=headers)
-	return req.json()
+    url = host + '/sendSeen'
+    data = {
+        'chat_id': to
+    }
+    req = requests.post(url, data=data, headers=headers)
+    return req.json()
 
 def sendReply(message_id, text):
-	url = host + '/sendReply'
-	data = {
-		'message_id': message_id,
-		'message': text
-	}
-	req = requests.post(url, data=data, headers=headers)
-	return req.json()
+    url = host + '/sendReply'
+    data = {
+        'message_id': message_id,
+        'message': text
+    }
+    req = requests.post(url, data=data, headers=headers)
+    return req.json()
 
 def downloadObjMessage(message_id):
-	url = host + '/messages/' + message_id + '/download'
-	req = requests.get(url, headers=headers)
-	return req.json()
+    url = host + '/messages/' + message_id + '/download'
+    req = requests.get(url, headers=headers)
+    return req.json()
 
 def loadChat(to):
-	url = host + '/chatLoadEarlier'
-	data = {
-		'chat_id': to
-	}
-	req = requests.post(url, data=data, headers=headers)
-	return req.json()
+    url = host + '/chatLoadEarlier'
+    data = {
+        'chat_id': to
+    }
+    req = requests.post(url, data=data, headers=headers)
+    return req.json()
 
 def loadAllChat(to):
-	url = host + '/chatLoadAllEarlier'
-	data = {
-		'chat_id': to
-	}
-	req = requests.post(url, data=data, headers=headers)
-	return req.json()
+    url = host + '/chatLoadAllEarlier'
+    data = {
+        'chat_id': to
+    }
+    req = requests.post(url, data=data, headers=headers)
+    return req.json()
 
 def loadChatStatus(to):
-	url = host + '/chatLoadStatus'
-	data = {
-		'chat_id': to
-	}
-	req = requests.post(url, data=data, headers=headers)
-	return req.json()
+    url = host + '/chatLoadStatus'
+    data = {
+        'chat_id': to
+    }
+    req = requests.post(url, data=data, headers=headers)
+    return req.json()
 
 def getBatteryLevel():
-	url = host + '/getBatteryLevel'
-	req = requests.get(url, headers=headers)
-	return req.json()
+    url = host + '/getBatteryLevel'
+    req = requests.get(url, headers=headers)
+    return req.json()
 
 def deleteChat(to):
-	url = host + '/deleteChat'
-	data = {
-		'chat_id': to
-	}
-	req = requests.post(url, data=data, headers=headers)
-	return req.json()
+    url = host + '/deleteChat'
+    data = {
+        'chat_id': to
+    }
+    req = requests.post(url, data=data, headers=headers)
+    return req.json()
 
 def unsendMessage(to, message_ids=[], _all=True):
     url = host + '/unsendMessage'
@@ -184,43 +184,43 @@ def unsendMessage(to, message_ids=[], _all=True):
     return req.json()
 
 def getGroupParticipantsIds(to):
-	url = host + '/getGroupParticipantsIds'
-	data = {
-		'group_id': to
-	}
-	req = requests.post(url, data=data, headers=headers)
-	return req.json()
+    url = host + '/getGroupParticipantsIds'
+    data = {
+        'group_id': to
+    }
+    req = requests.post(url, data=data, headers=headers)
+    return req.json()
 
 def getGroupParticipants(to):
-	url = host + '/getGroupParticipants'
-	data = {
-		'group_id': to
-	}
-	req = requests.post(url, data=data, headers=headers)
-	return req.json()
+    url = host + '/getGroupParticipants'
+    data = {
+        'group_id': to
+    }
+    req = requests.post(url, data=data, headers=headers)
+    return req.json()
 
 def getGroupAdminsIds(to):
-	url = host + '/getGroupAdminIds'
-	data = {
-		'group_id': to
-	}
-	req = requests.post(url, data=data, headers=headers)
-	return req.json()
+    url = host + '/getGroupAdminIds'
+    data = {
+        'group_id': to
+    }
+    req = requests.post(url, data=data, headers=headers)
+    return req.json()
 
 def getGroupAdmins(to):
-	url = host + '/getGroupAdmins'
-	data = {
-		'group_id': to
-	}
-	req = requests.post(url, data=data, headers=headers)
-	return req.json()
+    url = host + '/getGroupAdmins'
+    data = {
+        'group_id': to
+    }
+    req = requests.post(url, data=data, headers=headers)
+    return req.json()
 
 def mentionAll(message):
-
-    if message['chatType'] == 'group':
+    to = message['chatId']
+    if message['chat']['isGroup']:
         result = '╭───「 Mention Members 」\n'
         no = 0
-        members = getGroupParticipantsIds(message['chat_id'])['result']
+        members = getGroupParticipantsIds(message['chatId'])['result']
         if myId in members:
             members.remove(myId)
         for member in members:
